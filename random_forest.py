17c0ad15df6160036dd960a6da2b70aab6026661
aamui <aaronrubenmui@gmail.com>
2025-12-08 22:11:56 -0800

    typos

--- random_forest.py
+++ random_forest.py
@@ -15,6 +15,8 @@ def main():
     parser.add_argument("--pca_components", type=int, default=100, help="Number of PCA components")
     parser.add_argument('--n_estimators', type=int, default = 100, help = 'Number of trees in forest')
     parser.add_argument('--criterion', type = str, default = 'gini', help = 'Split criterion')
+    parser.add_argument('--max_depth', type = int, default = 15, help = 'Tree max depth')
+    parser.add_argument('--min_samples_leaf', type=int, default = 5, help = 'Min samples per leaf')
     args = parser.parse_args()
 
     # loading data
@@ -23,7 +25,7 @@ def main():
     test_dataset = datasets.SVHN(root='./data', split='test', download=True, transform = transform)
     
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=args.batch_siez, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
 
     # reshape training
     X_train = []
@@ -44,17 +46,24 @@ def main():
     y_test = np.concatenate(y_test)
 
     # pca projection
+    print('Doing PCA')
     pca = PCA(n_components = args.pca_components)
     X_train_pca = pca.fit_transform(X_train)
     X_test_pca = pca.transform(X_test)
 
-    clf = RandomForestClassifier(n_estimators = args.n_estimators, criterion = args.criterion)
+    print('Fitting random forest')
+    clf = RandomForestClassifier(n_estimators = args.n_estimators, criterion = args.criterion, max_depth = args.max_depth)
     clf.fit(X_train_pca, y_train)
 
-    rf_acc = clf.score(X_test_pca, y_test)
-    rf_err = 1. - rf_acc
-    print(f'Random forest error on {args.pca_components} PCA components: {rf_err:.3f}')
+    rf_train_acc = clf.score(X_train_pca, y_train)
+    rf_train_err = 1. - rf_train_acc
+    print(f'Random forest train error on {args.pca_components} PCA components: {rf_train_err:.3f}')
 
+    rf_test_acc = clf.score(X_test_pca, y_test)
+    rf_test_err = 1. - rf_test_acc
+    print(f'Random forest test error on {args.pca_components} PCA components: {rf_test_err:.3f}')
+    print('Args:')
+    print(vars(args))
 
 if __name__ == '__main__':
     main()
